b03d479be3472ba0147c871a174e08ff2da16c4b
Purvig648 <97182329+Purvig648@users.noreply.github.com>
2023-10-12 22:45:09 +0530

    Add files via upload

---
+++ assignment2d.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		age    int     = 21
+		name   string  = "Purvi"
+		height float32 = 5.12
+	)
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(height)
+}
---
+++ assignment2e.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const i int = 12
+const j float32 = 3.11
+const s string = "purvi"
+const k bool = true
+
+func main() {
+	fmt.Println(i, j, s, k)
+	/*i = 12
+	j = 5.89
+	s = "shivu"
+	k = false*/
+}
