f29cdfef649c9be8d0b17c66c4b0af4fcc6cc698
Purvig648 <97182329+Purvig648@users.noreply.github.com>
2023-10-12 22:43:52 +0530

    Add files via upload

---
+++ assignment1.go
@@ -0,0 +1,7 @@
+package assignments
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello,world!")
+}
---
+++ assignment2b.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a float64 = 3.14
+	fmt.Println(a)
+}
