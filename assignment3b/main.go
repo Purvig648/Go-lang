913d18e98cee36806ecb45bd0e7e57ce574f1065
Purvi <purvig648@gmail.com>
2023-10-13 09:18:08 +0530

    Changed

---
+++ assignment3b/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"assignments/assignment3b/temperaturee"
+	"fmt"
+)
+
+func main() {
+	var temp float64
+	fmt.Print("Enter the temperature ")
+	fmt.Scan(&temp)
+	i := temperaturee.Convert(temp)
+
+	fmt.Println("The Fahrenheit value is: ", i)
+}
